aaa96eae4ffef63721a26f8532211c36af4488a7
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2024-10-18 18:59:26 -0400

    idk

--- Sources/Prism/ElementBuilder.swift
+++ Sources/Prism/ElementBuilder.swift
@@ -8,22 +8,12 @@ public enum ElementBuilder {
 
     // MARK: Block Builders
 
-    public static func buildPartialBlock(first: [any PrismElement]) -> [any PrismElement] {
-        first
+    public static func buildBlock(_ components: PrismElement...) -> [PrismElement] {
+        components
     }
     
-    public static func buildPartialBlock(accumulated: [any PrismElement], next: [any PrismElement]) -> [any PrismElement] {
-        accumulated + next
-    }
-    
-    // MARK: Expression Builders
-
-    public static func buildExpression(_ expression: PrismElementConvertible) -> PrismElement {
-        expression.prismElement
-    }
-
-    public static func buildExpression(_ expression: [PrismElementConvertible]) -> [PrismElement] {
-        expression.map { $0.prismElement }
+    public static func buildBlock(_ components: PrismElementConvertible...) -> [PrismElement] {
+        components.map(\.prismElement)
     }
 
     // MARK: Conditional Builders
@@ -36,14 +26,6 @@ public enum ElementBuilder {
         components
     }
 
-    public static func buildEither(first components: [PrismElement]...) -> [PrismElement] {
-        components.flatMap { $0 }
-    }
-
-    public static func buildEither(second components: [PrismElement]...) -> [PrismElement] {
-        components.flatMap { $0 }
-    }
-    
     // MARK: Optionnal Builders
     
     public static func buildOptional(_ component: [PrismElement]?) -> [PrismElement] {
