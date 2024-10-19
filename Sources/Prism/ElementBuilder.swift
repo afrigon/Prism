b08ed93ac89b7555cbca5b7aa17ad5b1ecbecb1c
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2024-10-19 04:04:27 -0400

    idk

--- Sources/Prism/ElementBuilder.swift
+++ Sources/Prism/ElementBuilder.swift
@@ -8,31 +8,47 @@ public enum ElementBuilder {
 
     // MARK: Block Builders
 
-    public static func buildBlock(_ components: PrismElement...) -> [PrismElement] {
-        components
+    public static func buildPartialBlock(first: PrismElement) -> PrismElement {
+        first
     }
     
-    public static func buildBlock(_ components: [PrismElement]...) -> [PrismElement] {
-        components.flatMap { $0 }
+    public static func buildPartialBlock(accumulated: [PrismElement], next: PrismElement) -> [PrismElement] {
+        accumulated + [next]
     }
-
-    public static func buildBlock(_ components: PrismElementConvertible...) -> [PrismElement] {
-        components.map(\.prismElement)
-    }
-
-    // MARK: Conditional Builders
-
-    public static func buildEither(first components: PrismElement...) -> [PrismElement] {
-        components
-    }
-
-    public static func buildEither(second components: PrismElement...) -> [PrismElement] {
-        components
-    }
-
-    // MARK: Optionnal Builders
     
-    public static func buildOptional(_ component: [PrismElement]?) -> [PrismElement] {
-        component ?? []
+    public static func buildPartialBlock(first: PrismElementConvertible) -> PrismElement {
+        first.prismElement
     }
+    
+    public static func buildPartialBlock(accumulated: [PrismElement], next: PrismElementConvertible) -> [PrismElement] {
+        accumulated + [next.prismElement]
+    }
+
+//    public static func buildBlock(_ components: PrismElement...) -> [PrismElement] {
+//        components
+//    }
+//    
+//    public static func buildBlock(_ components: [PrismElement]...) -> [PrismElement] {
+//        components.flatMap { $0 }
+//    }
+//
+//    public static func buildBlock(_ components: PrismElementConvertible...) -> [PrismElement] {
+//        components.map(\.prismElement)
+//    }
+//    
+//    // MARK: Conditional Builders
+//
+//    public static func buildEither(first components: PrismElement...) -> [PrismElement] {
+//        components
+//    }
+//
+//    public static func buildEither(second components: PrismElement...) -> [PrismElement] {
+//        components
+//    }
+//
+//    // MARK: Optionnal Builders
+//    
+//    public static func buildOptional(_ component: [PrismElement]?) -> [PrismElement] {
+//        component ?? []
+//    }
 }
